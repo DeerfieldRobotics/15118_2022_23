3bee5c0caa72bdbf1db3ca1f44a197571d5682c8
James X <xtremejames1@gmail.com>
2022-10-05 21:26:20 -0500

    added back teleop and drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.utils.Drivetrain;
+
+@TeleOp(name = "teleop", group = "teleop")
+public class teleop extends LinearOpMode {
+    public ElapsedTime runtime = new ElapsedTime();
+
+    private Drivetrain drivetrain;
+
+    private DcMotor getMotor(String motorName) {
+        return hardwareMap.get(DcMotor.class, motorName);
+    }
+    public ElapsedTime loopTime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        initialize();
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()) {
+            double loopStart = loopTime.milliseconds();
+            if(gamepad1.left_stick_x != 0 || gamepad1.right_stick_x != 0 || gamepad1.left_stick_y != 0)
+            {
+                drivetrain.move(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.left_stick_y);
+            }
+            else {
+                drivetrain.stop();
+            }
+        }
+    }
+
+    public void initialize() {
+        drivetrain = new Drivetrain(hardwareMap);
+    }
+}
