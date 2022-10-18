3c2018447fe24d3f697f38794878a55b71852ba1
James X <xtremejames1@gmail.com>
2022-10-18 20:24:46 +0000

    added specific shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvpipelines/ConeDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvpipelines/ConeDetectionPipeline.java
@@ -23,7 +23,50 @@ public class ConeDetectionPipeline extends OpenCvPipeline {
             return input;
         }
 
-        Imgproc.cvtColor(workingMatrix, workingMatrix, Imgproc.COLOR_RGB2YCrCb);
+        Imgproc.cvtColor(workingMatrix, workingMatrix, Imgproc.COLOR_RGB2YCrCb); //converts to ycrcb colorspace
+
+        //create rectangles, for loop
+        double[][][] initial = new double[54][96][2] ;
+        double cbTotal = 0;
+        double crTotal = 0;
+        double cbStandardDev = 0;
+        double crStandardDev = 0;
+        for(int i = 0; i<initial.length;i++) { //collects all cr cb values values
+            for(int j = 0; j<initial[0].length;j++) {
+                initial[i][j][0]=Core.sumElems(workingMatrix.submat(j*1920/initial[0].length,(j+1)*1920/initial[0].length),i*1080/initial.length,(i+1)*1080/initial.length).val[2]; //finds sum of submat and gets cb value
+                cbTotal+=initial[i][j][0];
+                initial[i][j][1]=Core.sumElems(workingMatrix.submat(j*1920/initial[0].length,(j+1)*1920/initial[0].length),i*1080/initial.length,(i+1)*1080/initial.length).val[3]; //finds sum of submat and gets cr value
+                crTotal+=initial[i][j][1];
+            }
+        }
+        final double cbMean = cbTotal/(initial.length*initial[0].length);
+        final double crMean = crTotal/(initial.length*initial[0].length);
+
+        //standard deviation:
+        
+        for(int i = 0; i<initial.length;i++) {
+            for(int j = 0;j<initial[0].length;j++) {
+                cbStandardDev+=Math.pow((initial[i][j][0]-cbMean),2);
+                crStandardDev+=Math.pow((initial[i][j][1]-crMean),2);
+            }
+        }
+
+        cbStandardDev=Math.sqrt(cbStandardDev/(initial.length*initial[0].length));
+        crStandardDev=Math.sqrt(crStandardDev/(initial.length*initial[0].length));
+
+        for(int i = 0; i<initial.length;i++) {
+            for(int j = 0;j<initial[0].length;j++) {
+                
+            }
+        }
+        //identify outliers with mean and threshold for standard deviation thing
+        //store outlier location somehow, maybe boolean array, or just array of points
+        //iterate using squares found with above
+
+
+        for(int i = 0; i<10;i++) {
+
+        }
 
         Mat matLeft = workingMatrix.submat(120, 150, 10, 50);
         Mat matCenter = workingMatrix.submat(120, 150, 80, 120);
