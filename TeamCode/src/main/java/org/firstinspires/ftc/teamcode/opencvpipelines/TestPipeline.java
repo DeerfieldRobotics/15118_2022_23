427a5b28cce8e220f7b8a3c777669a8516a05fa7
James X <xtremejames1@gmail.com>
2022-10-19 09:24:37 -0500

    added shitty pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvpipelines/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvpipelines/TestPipeline.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.opencvpipelines;
 
+import com.vuforia.Rectangle;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Range;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
@@ -26,11 +30,39 @@ public class TestPipeline extends OpenCvPipeline {
 
         Core.inRange(workingMatrix, low, high, workingMatrix);
 
-        for(int x = 0; x<1920;x++) {
-            
+        int lowx = 0;
+        int highx = 0;
+        int x = 0;
+        while (x<1920) {
+            while (Core.sumElems(workingMatrix.submat(new Range(x, x + 1), new Range(0, 1079))).val[2] == 0) {
+                x++;
+                lowx = x;
+            }
+            while(Core.sumElems(workingMatrix.submat(new Range(x, x + 1), new Range(0, 1079))).val[2] > 0) {
+                x++;
+                highx = x;
+            }
         }
 
-        Imgproc.rectangle(workingMatrix, new Rectangle(), new Scalar(0,255,0));
+        int lowy = 0;
+        int highy = 0;
+        int y = 0;
+        while (y<1080) {
+            while (Core.sumElems(workingMatrix.submat(new Range(y, y + 1), new Range(0, 1919))).val[2] == 0) {
+                y++;
+                lowy = y;
+            }
+            while(Core.sumElems(workingMatrix.submat(new Range(y, y + 1), new Range(0, 1919))).val[2] > 0) {
+                y++;
+                highx = y;
+            }
+        }
+
+
+
+        Imgproc.rectangle(workingMatrix, new Rect(lowx, highx, lowy, highy), new Scalar(0,255,0));
+
+
 
         return workingMatrix;
     }
