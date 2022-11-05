474e3a06aadb456a5ed9a15f58bdfba3c7a5711b
Anthony <aretelew@gmail.com>
2022-11-05 10:07:15 -0500

    PID and IMU stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/AutoDrivetrainTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/AutoDrivetrainTester.java
@@ -1,68 +1,78 @@
 package org.firstinspires.ftc.teamcode.testers;
 
+import android.graphics.drawable.GradientDrawable;
+
+import com.acmerobotics.roadrunner.util.Angle;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.utils.AutoDrivetrain;
 
 @Autonomous(name = "AutoDrivetrainTester")
 public class AutoDrivetrainTester extends LinearOpMode {
 
-    private AutoDrivetrain d;
+    private AutoDrivetrain drivetrain;
     DcMotorEx fl, fr, bl, br;
+
 
     private final int ticksPerInch = 22;
 
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
-
         waitForStart();
 
         while(opModeIsActive()) {
+            drivetrain.turn(-90);
+            telemetry.addData("Current Angle", drivetrain.getAngle());
+            //drivetrain.moveTicks(1000,0,0);
 
-            d.moveTicks(1000,0,0);
+            //telemetry.addData("Front Left Current", drivetrain.getCurrent()[0]);
+            //telemetry.addData("Front Right Current", drivetrain.getCurrent()[1]);
+            //telemetry.addData("Back Left Current", drivetrain.getCurrent()[2]);
+            //telemetry.addData("Back Right Current", drivetrain.getCurrent()[3]);
 
-            telemetry.addData("Front Left Current",d.getCurrent()[0]);
-            telemetry.addData("Front Right Current",d.getCurrent()[1]);
-            telemetry.addData("Back Left Current",d.getCurrent()[2]);
-            telemetry.addData("Back Right Current",d.getCurrent()[3]);
+            //telemetry.addData("Front Left Position", drivetrain.getCurrentPosition()[0]);
+            //telemetry.addData("Front Right Position", drivetrain.getCurrentPosition()[1]);
+            //telemetry.addData("Back Left Position", drivetrain.getCurrentPosition()[2]);
+            //telemetry.addData("Back Right Position", drivetrain.getCurrentPosition()[3]);
+            //telemetry.addData("Proportion", drivetrain.getPIDFCoefficients()[0][0]);
+            //telemetry.addData("Integral", drivetrain.getPIDFCoefficients()[0][1]);
+            //telemetry.addData("Derivative", drivetrain.getPIDFCoefficients()[0][2]);
+            //telemetry.addData("Feed Forward", drivetrain.getPIDFCoefficients()[0][3]);
+            //telemetry.update();
 
-            telemetry.addData("Front Left Position",d.getCurrentPosition()[0]);
-            telemetry.addData("Front Right Position",d.getCurrentPosition()[1]);
-            telemetry.addData("Back Left Position",d.getCurrentPosition()[2]);
-            telemetry.addData("Back Right Position",d.getCurrentPosition()[3]);
+            //Thread.sleep(5000);
+
+            //drivetrain.moveTicks(0,0,0);
+
+            //telemetry.addData("Front Left Current", drivetrain.getCurrent()[0]);
+            //telemetry.addData("Front Right Current", drivetrain.getCurrent()[1]);
+            //telemetry.addData("Back Left Current", drivetrain.getCurrent()[2]);
+            //telemetry.addData("Back Right Current", drivetrain.getCurrent()[3]);
+
+            //telemetry.addData("Front Left Position", drivetrain.getCurrentPosition()[0]);
+            //telemetry.addData("Front Right Position", drivetrain.getCurrentPosition()[1]);
+            //telemetry.addData("Back Left Position", drivetrain.getCurrentPosition()[2]);
+            //telemetry.addData("Back Right Position", drivetrain.getCurrentPosition()[3]);
+            //telemetry.addData("Proportion",d.getPIDFCoefficients()[0][0]);
+            //telemetry.addData("Integral",d.getPIDFCoefficients()[0][1]);
+            //telemetry.addData("Derivative",d.getPIDFCoefficients()[0][2]);
+            //telemetry.addData("Front Feed",d.getPIDFCoefficients()[0][3]);
             telemetry.update();
 
-            Thread.sleep(5000);
 
-            d.moveTicks(0,0,0);
-
-            telemetry.addData("Front Left Current",d.getCurrent()[0]);
-            telemetry.addData("Front Right Current",d.getCurrent()[1]);
-            telemetry.addData("Back Left Current",d.getCurrent()[2]);
-            telemetry.addData("Back Right Current",d.getCurrent()[3]);
-
-            telemetry.addData("Front Left Position",d.getCurrentPosition()[0]);
-            telemetry.addData("Front Right Position",d.getCurrentPosition()[1]);
-            telemetry.addData("Back Left Position",d.getCurrentPosition()[2]);
-            telemetry.addData("Back Right Position",d.getCurrentPosition()[3]);
-            telemetry.addData("Derivative",d.getPIDFCoefficients()[0][1]);
-            telemetry.update();
-
-
-            Thread.sleep(5000);
+            //Thread.sleep(5000);
 
         }
     }
 
     public void initialize() {
-        d = new AutoDrivetrain(hardwareMap);
+        drivetrain = new AutoDrivetrain(hardwareMap);
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AutoDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AutoDrivetrain.java
@@ -1,21 +1,47 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class AutoDrivetrain {
     DcMotorEx fl, fr, bl, br;
+
+    public BNO055IMU imu;
+    private Orientation lastAngles = new Orientation();
+    private double curAngle = 0.0;
 
     private final int ticksPerInch = 22;
 
     private final double speedMult = 0.8;
 
+    private final double kP = 10;
+    private final double kI = 0.1;
+    private final double kD = 1;
+    private final double ff = 2.5;
 
     public AutoDrivetrain(HardwareMap hw) {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hw.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
         fl = hw.get(DcMotorEx.class, "fl");
         fr = hw.get(DcMotorEx.class, "fr");
         bl = hw.get(DcMotorEx.class, "bl");
@@ -34,21 +60,24 @@ public class AutoDrivetrain {
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
 
-
+        fl.setVelocityPIDFCoefficients(kP,kI,kD,ff);
+        fr.setVelocityPIDFCoefficients(kP,kI,kD,ff);
+        bl.setVelocityPIDFCoefficients(kP,kI,kD,ff);
+        br.setVelocityPIDFCoefficients(kP,kI,kD,ff);
     }
 
     public void moveTicks(int forward, int strafe, int turn) {
-        fl.setTargetPosition(forward-turn-strafe);
-        fr.setTargetPosition(forward+turn+strafe);
-        bl.setTargetPosition(forward-turn+strafe);
-        br.setTargetPosition(forward+turn-strafe);
+        fl.setTargetPosition(forward - turn - strafe);
+        fr.setTargetPosition(forward + turn + strafe);
+        bl.setTargetPosition(forward - turn + strafe);
+        br.setTargetPosition(forward + turn - strafe);
 
         fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        double speed = 1*speedMult;
+        double speed = 1 * speedMult;
 
         fl.setPower(speed);
         fr.setPower(speed);
@@ -60,6 +89,71 @@ public class AutoDrivetrain {
         moveTicks(ticksPerInch*(int)forward, ticksPerInch*(int)strafe, ticksPerInch*(int)turn);
     }
 
+    public void setMotorPower(double fl, double fr, double bl, double br) {
+        this.fl.setPower(fl);
+        this.fr.setPower(fr);
+        this.bl.setPower(bl);
+        this.br.setPower(br);
+    }
+
+    public void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        curAngle = 0;
+    }
+
+    public double getAngle() {
+        Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double deltaAngle = orientation.firstAngle - lastAngles.firstAngle;
+
+        if(deltaAngle > 180) {
+            deltaAngle -= 360;
+        } else if(deltaAngle <= -180) {
+            deltaAngle += 360;
+        }
+
+        curAngle += deltaAngle;
+        lastAngles = orientation;
+
+        return curAngle;
+    }
+
+    public void turn(double degrees) {
+        resetAngle();
+
+        double error = degrees;
+
+        while(Math.abs(error) > 2) {
+            double motorPower;
+            if(error < 0) {
+                motorPower = -0.3;
+            } else {
+                motorPower = 0.3;
+            }
+            setMotorPower(-motorPower, motorPower, -motorPower, motorPower);
+            error = degrees - getAngle();
+            //telemetry.addData("Error", error);
+            //telemetry.update();
+        }
+        this.fl.setPower(0);
+        this.fr.setPower(0);
+        this.bl.setPower(0);
+        this.br.setPower(0);
+    }
+
+    public void turnTo(double degrees) {
+        Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double error = degrees - orientation.firstAngle;
+
+        if(error >  180) {
+            error -= 360;
+        } else if(error < -180) {
+            error += 360;
+        }
+
+        turn(error);
+    }
+
     public double[] getCurrent() {
         return new double[]{fl.getCurrent(CurrentUnit.AMPS),fr.getCurrent(CurrentUnit.AMPS),bl.getCurrent(CurrentUnit.AMPS),br.getCurrent(CurrentUnit.AMPS)};
     }
@@ -69,9 +163,9 @@ public class AutoDrivetrain {
     }
 
     public double[][] getPIDFCoefficients() {
-        return new double[][]{{fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d}
-                            ,{fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d}
-                            ,{bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d}
-                            ,{br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d}};
+        return new double[][]{{fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d, fl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).f}
+                            ,{fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d, fr.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).f}
+                            ,{bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d, bl.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).f}
+                            ,{br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).p,br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).i, br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).d, br.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION).f}};
     }
 }
