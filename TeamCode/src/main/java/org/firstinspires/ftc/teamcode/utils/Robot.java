33815b1bfe5277bdf0b2e81c29f1a5299881c84f
kevins4202 <93951122+kevins4202@users.noreply.github.com>
2022-11-10 10:56:39 -0600

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -4,9 +4,14 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Robot {
+    
+    public Drivetrain drivetrain;
+    public Slide slide;
+    
     public Robot(HardwareMap hwMap) {
-        Drivetrain drivetrain = new Drivetrain(hwMap);
-        Slide slide = new Slide(hwMap);
+        drivetrain = new Drivetrain(hwMap);
+        slide = new Slide(hwMap);
         // -> INSERT INTAKE CLASS
     }
+    
 }
