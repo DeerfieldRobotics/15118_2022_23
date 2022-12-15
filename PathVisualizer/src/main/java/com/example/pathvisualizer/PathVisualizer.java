287516980246af28c5abdbc67a0a6387e2924ba0
tonythedrony <aretelew@gmail.com>
2022-12-15 17:29:57 -0600

    auto

--- PathVisualizer/src/main/java/com/example/pathvisualizer/PathVisualizer.java
+++ PathVisualizer/src/main/java/com/example/pathvisualizer/PathVisualizer.java
@@ -9,14 +9,14 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class PathVisualizer {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(900);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         //Trajectory strafeRight = new Trajectory()
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setDimensions(15,14)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 //.setStartPose(new Pose2d(50, 50))
-                .setConstraints(30, 30, Math.toRadians(180), Math.toRadians(180), 14.55)
+                .setConstraints(35, 40, Math.toRadians(180), Math.toRadians(180), 14.55)
                 .followTrajectorySequence(drive ->
 
 //                        drive.trajectorySequenceBuilder(new Pose2d(-34,63, Math.toRadians(180)))
@@ -43,37 +43,67 @@ public class PathVisualizer {
 //
 //                                .lineToSplineHeading(new Pose2d(-34, 11, Math.toRadians(180)))
 //                                .build()
-
+//                              };
 
                                 drive.trajectorySequenceBuilder(new Pose2d(34,63, Math.toRadians(180)))
                                         // LEFT BLUE
                                         .strafeLeft(3)
-                                        .forward(5)
-//                                        .splineTo(new Vector2d(14,45) ,Math.toRadians(-90))
-                                        .splineTo(new Vector2d(14,20) ,Math.toRadians(-75))
+                                        .lineToSplineHeading(new Pose2d(28,60, Math.toRadians(180)))
                                         .splineTo(new Vector2d(16,14), Math.toRadians(-63))
                                         .forward(7)
-//                                        .splineToSplineHeading(new Pose2d(17,15,Math.toRadians(-30)),Math.toRadians(-30))
+
+
                                         .strafeLeft(3)
                                         .splineToSplineHeading(new Pose2d(44,11.5,Math.toRadians(0)), Math.toRadians(0))
                                         .forward(16)
 
-                                        .lineToSplineHeading(new Pose2d(40,11.5, Math.toRadians(-30)))
-                                        .lineToLinearHeading(new Pose2d(35,11.5,Math.toRadians(-135)))
-                                        .forward(8)
-                                        .back(5)
-                                        .lineToLinearHeading(new Pose2d(35,11.5,Math.toRadians(0)))
-                                        .forward(24)
+                                        .waitSeconds(0.2)
 
-//                                        .back(20)
-//                                        .lineToLinearHeading(new Pose2d(35,11.5,Math.toRadians(-135)))
-//                                        .forward(8)
-//                                        .back(5)
-//                                        .lineToLinearHeading(new Pose2d(35,11.5,Math.toRadians(0)))
-//                                        .forward(24)
+                                        .lineToSplineHeading(new Pose2d(40,11.5, Math.toRadians(135)))
+                                        .splineToSplineHeading(new Pose2d(29,6, Math.toRadians(-135)), Math.toRadians(-135))
+                                        .lineToSplineHeading(new Pose2d(47,11,Math.toRadians(90)))
+                                        .splineToSplineHeading(new Pose2d(60,11.5, Math.toRadians(0)), Math.toRadians(0))
+
+                                        .waitSeconds(0.2)
+
+                                        .lineToSplineHeading(new Pose2d(40,11.5, Math.toRadians(135)))
+                                        .splineToSplineHeading(new Pose2d(29,6, Math.toRadians(-135)), Math.toRadians(-135))
+                                        .lineToSplineHeading(new Pose2d(47,11,Math.toRadians(90)))
+                                        .splineToSplineHeading(new Pose2d(60,11.5, Math.toRadians(0)), Math.toRadians(0))
+                                        .waitSeconds(0.2)
+
+                                        .lineToSplineHeading(new Pose2d(40,11.5, Math.toRadians(135)))
+                                        .splineToSplineHeading(new Pose2d(29,6, Math.toRadians(-135)), Math.toRadians(-135))
 
                                         .build()
                 );
+
+//                                drive.trajectorySequenceBuilder(new Pose2d(-34,-63, Math.toRadians(0)))
+//                                        .strafeLeft(3)
+//                                        .lineToSplineHeading(new Pose2d(-31,-60, Math.toRadians(0)))
+//                                        .splineTo(new Vector2d(-16,-14), Math.toRadians(117))
+//                                        .forward(7)
+//
+//
+//                                        .strafeLeft(3)
+//                                        .splineToSplineHeading(new Pose2d(-44,-11.5,Math.toRadians(180)), Math.toRadians(180))
+//                                        .forward(16)
+//
+//                                        .waitSeconds(0.2)
+//
+//                                        .lineToSplineHeading(new Pose2d(-40,-11.5, Math.toRadians(-45)))
+//                                        .splineToSplineHeading(new Pose2d(-29,-6, Math.toRadians(45)), Math.toRadians(45))
+//                                        .lineToSplineHeading(new Pose2d(-40,-11.5,Math.toRadians(180)))
+//                                        .splineToSplineHeading(new Pose2d(-60,-13, Math.toRadians(170)), Math.toRadians(170))
+
+                                            //.waitSeconds(0.2)
+//
+//                                        .lineToSplineHeading(new Pose2d(-40,-11.5, Math.toRadians(-45)))
+//                                        .splineToSplineHeading(new Pose2d(-29,-6, Math.toRadians(45)), Math.toRadians(45))
+//
+//                                        .build()
+//
+//                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
