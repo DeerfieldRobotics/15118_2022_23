56ad85ad94e1960f12f4215f8d1626d4585328b1
tonythedrony <aretelew@gmail.com>
2023-01-14 08:50:13 -0600

    auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/rightAutoAsync.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/rightAutoAsync.java
@@ -10,8 +10,8 @@ import org.firstinspires.ftc.teamcode.utils.AprilTags;
 import org.firstinspires.ftc.teamcode.utils.RubberBandIntake;
 import org.firstinspires.ftc.teamcode.utils.Slide;
 
-@Autonomous(name = "LEFT_AUTO_ASYNC")
-public class leftAutoAsync extends OpMode {
+@Autonomous(name = "RIGHT_AUTO_ASYNC")
+public class rightAutoAsync extends OpMode {
     private SampleMecanumDrive drive;
     private AprilTags aprilTags;
     private int detectedTag;
@@ -23,7 +23,7 @@ public class leftAutoAsync extends OpMode {
     @Override
     public void init() {
         drive = new SampleMecanumDrive(hardwareMap);
-        aprilTags = new AprilTags(hardwareMap, "rightCam");
+        aprilTags = new AprilTags(hardwareMap, "leftCam");
         rubberBandIntake = new RubberBandIntake(hardwareMap);
         s = new Slide(hardwareMap);
     }
@@ -56,9 +56,9 @@ public class leftAutoAsync extends OpMode {
 //    }
 
     public void start() {
-        if (detectedTag == 1) {
+        if (detectedTag == 3) {
             path = drive.trajectorySequenceBuilder(startPose)
-                    .strafeRight(2)
+                    .strafeLeft(2)
                     .forward(28)
                     .addTemporalMarker(3, () -> {
                         telemetry.update();
@@ -72,19 +72,19 @@ public class leftAutoAsync extends OpMode {
                     //park
                     .waitSeconds(1)
                     .back(5)
-                    .strafeRight(30)
+                    .strafeLeft(30)
                     .forward(5)
                     .build();
         } else if (detectedTag == 2) {
             path = drive.trajectorySequenceBuilder(startPose)
-                    .strafeRight(2)
+                    .strafeLeft(2)
                     .forward(28)
-                    .addTemporalMarker(0.75, () -> {
+                    .addTemporalMarker(3, () -> {
                         telemetry.update();
                         rubberBandIntake.updatePower(-1);
                     })
 
-                    .addTemporalMarker(2.75, () -> {
+                    .addTemporalMarker(5, () -> {
                         rubberBandIntake.updatePower(0);
                         telemetry.update();
                         s.updateTarget(-600);
@@ -92,17 +92,17 @@ public class leftAutoAsync extends OpMode {
                     //park
                     .waitSeconds(2.5)
                     .back(21)
-                    .strafeRight(35)
+                    .strafeLeft(35)
                     .build();
         } else{
             path = drive.trajectorySequenceBuilder(startPose)
-                    .strafeRight(2)
+                    .strafeLeft(2)
                     .forward(28)
-                    .addTemporalMarker(0.75, () -> {
+                    .addTemporalMarker(3, () -> {
                         telemetry.update();
                         rubberBandIntake.updatePower(-1);
                     })
-                    .addTemporalMarker(2.75, () -> {
+                    .addTemporalMarker(5, () -> {
                         rubberBandIntake.updatePower(0);
                         telemetry.update();
                         s.updateTarget(-600);
@@ -110,7 +110,7 @@ public class leftAutoAsync extends OpMode {
                     //park
                     .waitSeconds(2.5)
                     .back(44)
-                    .strafeRight(32)
+                    .strafeLeft(32)
                     .build();
         }
 
