a210dea47751a9fd9d724c0c59f51f514e3d3a0e
James X <xtremejames1@gmail.com>
2023-01-18 22:58:19 -0600

    for andrew daddy 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/savingandrew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testers/savingandrew.java
@@ -2,27 +2,41 @@ package org.firstinspires.ftc.teamcode.testers;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.utils.Drivetrain;
 
 @Autonomous(name = "savingandrew")
 public class savingandrew extends LinearOpMode {
 
-    Drivetrain d = new Drivetrain(hardwareMap);
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        d.run_using_encoder();
-        d.floatMotors();
+
+        DcMotorEx fr = (DcMotorEx)hardwareMap.get(DcMotor.class, "fr");
+        DcMotorEx fl = (DcMotorEx)hardwareMap.get(DcMotor.class, "fl");
+        DcMotorEx br = (DcMotorEx)hardwareMap.get(DcMotor.class, "br");
+        DcMotorEx bl = (DcMotorEx)hardwareMap.get(DcMotor.class, "bl");
+
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         waitForStart();
 
         while(opModeIsActive()) {
-            telemetry.addData("fl", d.getMotorPositions()[0]);
-            telemetry.addData("fr", d.getMotorPositions()[1]);
-            telemetry.addData("bl", d.getMotorPositions()[2]);
-            telemetry.addData("br", d.getMotorPositions()[3]);
+            telemetry.addData("fl", fl.getCurrentPosition());
+            telemetry.addData("fr", fr.getCurrentPosition());
+            telemetry.addData("bl", bl.getCurrentPosition());
+            telemetry.addData("br", br.getCurrentPosition());
             telemetry.update();
 
         }
