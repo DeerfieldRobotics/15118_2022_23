115e7eddacbb0e240b0a25ab84788667ab587867
Anthony <aretelew@gmail.com>
2023-01-23 15:32:51 -0600

    continued auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -53,8 +53,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0193;
-    public static double kA = 0.0023;
+    public static double kV = 0.0175; //0.0193
+    public static double kA = 0.0035; //0.0023
     public static double kStatic = 0.0022;
 
     /*
@@ -64,7 +64,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
+    public static double MAX_VEL = 45;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = 3.64481;
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
